d2d948b9884251b5d3a1aaef9e1f2dcee00caf63
Hutlscot <vorontsov.r@bk.ru>
2020-03-13 15:33:10 +0300

    добавил страницу создания пользователей

--- Diplom/Pages/Add_user.xaml.cs
+++ Diplom/Pages/Add_user.xaml.cs
@@ -20,9 +20,44 @@ namespace Diplom
     /// </summary>
     public partial class Add_user : Page
     {
+        ConnectionEntity dbContext = new ConnectionEntity();
         public Add_user()
         {
             InitializeComponent();
+        }
+
+        private void btm_create_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person();
+            person.Name = txt_name.Text;
+            person.Type = "Пользователь";
+            person.LastName = txt_surname.Text;
+            person.MiddleName = txt_patronymic.Text;
+            dbContext.People.Add(person);
+            dbContext.SaveChanges();
+            User user = new User();
+            user.Id = person.Id;
+            user.Login = txt_login.Text;
+            user.Password = txt_password.Text;
+            user.TypeUserId = cmb_type.SelectedIndex+1;
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+            Dialog_message.MessageOK("Сохранено");
+            Transfer.Trans("Добавить пользователя");
+        }
+        private bool Validation()
+        {
+            if(Val.Val_txt(txt_name)||Val.Val_txt(txt_surname)||Val.Val_txt(txt_patronymic)
+                ||Val.Val_txt(txt_login)||Val.Val_txt(txt_password))
+            {
+                return false;
+            }
+            if (txt_login.Text.Length < 5 || txt_password.Text.Length<8)
+            {
+                Dialog_message.MessageER("Некорретные\nданные");
+                return false;
+            }
+            return true;
         }
     }
 }
